d6ed7a0be2f6e04aa814cc3746886058df3f42aa
jerard <qbin221@gmail.com>
2019-11-21 12:44:29 -0500

    do what it says function completed. Now appends search results to log.txt

--- liri.js
+++ liri.js
@@ -33,7 +33,7 @@ switch (command) {
         break;
 
     case "do-what-it-says":
-
+        doWhatItSays();
         break;
 
     default:
@@ -54,7 +54,13 @@ function concertThis(input) {
                 console.log(`Venue: ${data[key].venue.name}`);
                 console.log(`Location: ${data[key].venue.city}, ${data[key].venue.region}`)
                 console.log(`Date: ${data[key].datetime}`)
-                console.log("////////////////////////////");
+                console.log("\n");
+
+                fs.appendFile("log.txt", "Band: " + input + "\nVenue: " + data[key].venue.name + "\nLocation: " + data[key].venue.city + "," + data[key].venue.region + "\nDate: " + data[key].datetime + "\n\n", function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                })
             }
 
         }).catch(function (error) {
@@ -75,7 +81,8 @@ function concertThis(input) {
 function movieThis(input) {
 
     if (!input) {
-        axios.get(`http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy`).then(function (response) {
+        input = "Mr Nobody"
+        axios.get(`http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`).then(function (response) {
 
             console.log(`Title: ${response.data.Title}`);
             console.log(`Release Date: ${response.data.Released}`);
@@ -85,6 +92,12 @@ function movieThis(input) {
             console.log(`Language: ${response.data.Language}`);
             console.log(`Plot: ${response.data.Plot}`);
             console.log(`Starring: ${response.data.Actors}`);
+
+            fs.appendFile("log.txt", "Title: " + response.data.Title + "\nRelease Date: " + response.data.Released + "\nIMDB Rating: " + response.data.Ratings[0].Value + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nProduced in: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " + response.data.Actors + "\n\n", function (err) {
+                if (err) {
+                    throw err;
+                }
+            })
 
         }).catch(function (error) {
             if (error.response) {
@@ -111,6 +124,13 @@ function movieThis(input) {
             console.log(`Plot: ${response.data.Plot}`);
             console.log(`Starring: ${response.data.Actors}`);
 
+            fs.appendFile("log.txt", "Title: " + response.data.Title + "\nRelease Date: " + response.data.Released + "\nIMDB Rating: " + response.data.Ratings[0].Value + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nProduced in: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " + response.data.Actors + "\n\n", function (err) {
+                if (err) {
+                    throw err;
+                }
+            })
+
+
         }).catch(function (error) {
             if (error.response) {
                 console.log(error.response.data);
@@ -136,6 +156,12 @@ function spotifyThisSong(input) {
                 console.log(`Song: ${data.name}`);
                 console.log(`Link: ${data.external_urls.spotify}`);
                 console.log(`Album: ${data.album.name}`);
+
+                fs.appendFile("log.txt", "Artist: " + data.artists[0].name + "\nSong :" + data.name + "\nLink :" + data.external_urls.spotify + "\nAlbum :" + data.album.name + "\n\n", function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                })
             })
             .catch(function (err) {
                 console.error("Error occurred: " + err);
@@ -149,6 +175,41 @@ function spotifyThisSong(input) {
             console.log(`Song: ${data.tracks.items[0].name}`);
             console.log(`Link: ${data.tracks.items[0].external_urls.spotify}`);
             console.log(`Album: ${data.tracks.items[0].album.name}`);
+
+            fs.appendFile("log.txt", "Artist: " + data.tracks.items[0].artists[0].name + "\nSong :" + data.tracks.items[0].name + "\nLink :" + data.tracks.items[0].external_urls.spotify + "\nAlbum :" + data.tracks.items[0].album.name + "\n\n", function (err) {
+                if (err) {
+                    throw err;
+                }
+            })
         })
     }
+}
+
+function doWhatItSays() {
+
+    fs.readFile("random.txt", "utf8", function (err, data) {
+
+        if (err) {
+            throw err;
+        }
+
+        const fileArray = data.split(",")
+        const command = fileArray[0];
+        const input = fileArray[1];
+
+        switch (command) {
+
+            case "concert-this":
+                concertThis(input);
+                break;
+
+            case "spotify-this-song":
+                spotifyThisSong(input);
+                break;
+
+            case "movie-this":
+                movieThis(input);
+                break;
+        }
+    })
 }
